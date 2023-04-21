ed3a695787d9aec9568f55f3a3cdb2aedb454225
Steven Lee <sehyun493@gmail.com>
2023-04-21 12:08:47 -0400

    Did Cart function and route.

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -1,19 +1,20 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import AuthForm from '../features/auth/AuthForm';
-import SignUpAuthForm from '../features/auth/SignUpAuthForm';
-import Home from '../features/home/Home';
-import { me } from './store';
-import SingleProduct from './components/SingleProduct';
-import AllProducts from './components/AllProducts';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import AuthForm from "../features/auth/AuthForm";
+import SignUpAuthForm from "../features/auth/SignUpAuthForm";
+import Home from "../features/home/Home";
+import { me } from "./store";
+import SingleProduct from "./components/SingleProduct";
+import AllProducts from "./components/AllProducts";
+import Cart from "./components/Order";
 
 /**
  * COMPONENT
  */
 
 const AppRoutes = () => {
-  const isLoggedIn = useSelector(state => !!state.auth.me.id);
+  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -24,26 +25,28 @@ const AppRoutes = () => {
     <div>
       {isLoggedIn ? (
         <Routes>
-          <Route path='/*' element={<Home />} />
-          <Route to='/home' element={<Home />} />
+          <Route path="/*" element={<Home />} />
+          <Route to="/home" element={<Home />} />
         </Routes>
       ) : (
         <Routes>
           <Route
-            path='/*'
-            element={<AuthForm name='login' displayName='Login' />}
+            path="/*"
+            element={<AuthForm name="login" displayName="Login" />}
           />
           <Route
-            path='/login'
-            element={<AuthForm name='login' displayName='Login' />}
+            path="/login"
+            element={<AuthForm name="login" displayName="Login" />}
           />
           <Route
-            path='/signup'
-            element={<SignUpAuthForm name='signup' displayName='Sign Up' />}
+            path="/signup"
+            element={<SignUpAuthForm name="signup" displayName="Sign Up" />}
           />
-          <Route path='/products/' element={<AllProducts />} />
+          <Route path="/products/" element={<AllProducts />} />
 
-          <Route path='/products/:productId/*' element={<SingleProduct />} />
+          <Route path="/products/:productId/*" element={<SingleProduct />} />
+
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       )}
     </div>
