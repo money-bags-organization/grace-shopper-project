0b450e1b7ce1b7dce9758ce7ceb49382a0fb759e
apophis51 <malcolmxvernon@hotmail.com>
2023-04-21 16:12:46 -0400

    addproducts to cart functionality in the slice
    	modified:   client/app/components/SingleProduct.js
    	modified:   client/features/singleProductSlice.js
    	modified:   server/auth/index.js

--- client/app/components/SingleProduct.js
+++ client/app/components/SingleProduct.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import {
   fetchSingleProductAsync,
   selectSingleProduct,
+  addtoCart
 } from '../../features/singleProductSlice';
 
 const SingleProduct = () => {
@@ -16,14 +17,24 @@ const SingleProduct = () => {
     dispatch(fetchSingleProductAsync(productId));
   }, [dispatch, productId]);
 
+  //  //added by malcolm david
+  //  const handleAddtoCart = id => {
+  //    dispatch(addToCart(id));
+  // };
+
   console.log('message', product.name);
+
+  let cart = {name:product.products.name,
+            price:product.products.price}
 
   return (
     <div>
       {/* <img src={campus.imageUrl} className='campus-Images' /> */}
-      <h1> {product.name} </h1>
-      <h1> {product.price} </h1>
-      <p>{product.quantity}</p>
+      <h1> {product.products.name} </h1>
+      <h1> {product.products.price} </h1>
+      <p>{product.products.quantity}</p>
+      <button type='button' onClick={() => dispatch(addtoCart(cart))}>
+                add to cart </button>
     </div>
   );
 };
