c6312950a4ceb3bfc5d3d68110b2c86fec191658
Steven Lee <sehyun493@gmail.com>
2023-04-21 16:09:45 -0400

    Cart is now rendering

--- client/features/auth/cart.js
+++ client/features/auth/cart.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Order from "./components/Order";
+import Order from "../../app/components/Order";
 
 function Cart() {
   const cartProducts = Order.cart;
-  if (cartProducts.length === 0) {
+  if (cartProducts && cartProducts.length === 0) {
     return <p>Your cart is empty</p>;
   }
 
@@ -11,14 +11,15 @@ function Cart() {
     <div>
       <h2>Your Cart</h2>
       <ul>
-        {cartProducts.map((item) => {
-          <li key={item.id}>
-            {item.name} - {item.quantity} x {item.price} ={" "}
-            {item.quantity * item.price}
-          </li>;
-        })}
+        {cartProducts &&
+          cartProducts.map((item) => {
+            <li key={item.id}>
+              {item.name} - {item.quantity} x {item.price} ={" "}
+              {item.quantity * item.price}
+            </li>;
+          })}
       </ul>
-      <p>Total: {cartProducts.totalCart()}</p>
+      <p>Total: {cartProducts && cartProducts.totalCart()}</p>
     </div>
   );
 }
