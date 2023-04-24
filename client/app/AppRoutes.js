aadf1a51213a0b116b0b42f1fa1c8e4418cf171a
apophis51 <malcolmxvernon@hotmail.com>
2023-04-24 09:42:01 -0400

    Restricted non admins from deleting or adding products.
    Included routes for non admins, and non users
    to view products without ability to edit or delete
    
    created additional seeds to test adding items
    to cart (aka orderproducts/orders)
    
    Files modified:
    	modified:   client/app/AppRoutes.js
    	new file:   client/app/components/NotAdmin_AllProducts.js
    	modified:   client/features/navbar/Navbar.js
    	modified:   script/seed.js
    	modified:   server/db/models/OrderProducts.js
    	modified:   server/db/models/Orders.js

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -7,6 +7,7 @@ import Home from "../features/home/Home";
 import { me } from "./store";
 import SingleProduct from "./components/SingleProduct";
 import AllProducts from "./components/AllProducts";
+import NotAdmin_AllProducts from "./components/NotAdmin_AllProducts";
 import Cart from "../features/auth/cart";
 
 /**
@@ -16,6 +17,11 @@ import Cart from "../features/auth/cart";
 const AppRoutes = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.me.id);
   const dispatch = useDispatch();
+
+  //*Malcolm was Here
+  const isLoggedAdmin = useSelector((state) => state.auth.me.isAdmin);
+
+  //End Malcolm edit
 
   useEffect(() => {
     dispatch(me());
@@ -27,6 +33,7 @@ const AppRoutes = () => {
         <Routes>
           <Route path="/*" element={<Home />} />
           <Route to="/home" element={<Home />} />
+          <Route path="/products/:productId/*" element={<SingleProduct />} />
         </Routes>
       ) : (
         <Routes>
@@ -42,13 +49,18 @@ const AppRoutes = () => {
             path="/signup"
             element={<SignUpAuthForm name="signup" displayName="Sign Up" />}
           />
-          <Route path="/products/" element={<AllProducts />} />
-
           <Route path="/products/:productId/*" element={<SingleProduct />} />
-
           <Route path="/cart" element={<Cart />} />
         </Routes>
       )}
+      {isLoggedAdmin ? 
+      <Routes>
+      <Route path="/products/" element={<AllProducts />} />
+      <Route path="/public/products/" element={<NotAdmin_AllProducts />} />
+     </Routes> 
+      :  (<Routes>
+      <Route path="/public/products/" element={<NotAdmin_AllProducts />} />
+     </Routes> )}
     </div>
   );
 };
