4adc9c1a7c5d5a19f82cdddada892b1cddfc4bbd
Joseph-WhiteSWE <white.joseph.jr@gmail.com>
2023-04-25 13:42:27 -0500

    Modified the CSS for AllProducts, NotAdmin and SingleProduct Changes to be committed: modified:   client/app/components/AllProducts.js modified:   client/app/components/NotAdmin_AllProducts.js modified:   client/app/components/SingleProduct.js modified:   public/index.html modified:   public/style.css

--- client/app/components/SingleProduct.js
+++ client/app/components/SingleProduct.js
@@ -21,40 +21,40 @@ const SingleProduct = () => {
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
   const [quantity, setQuantity] = useState('');
-//Malcolm added fetch all orders for cart
-const orderlength = useSelector((state) => state.singleProduct.orders.length) + 1
-console.log("startlength", orderlength)
-
+  //Malcolm added fetch all orders for cart
+  const orderlength =
+    useSelector(state => state.singleProduct.orders.length) + 1;
+  console.log('startlength', orderlength);
 
   useEffect(() => {
     dispatch(fetchSingleProductAsync(productId));
-    dispatch(fetchAllOrders())
-    dispatch(fetchOrderProducts())
+    dispatch(fetchAllOrders());
+    dispatch(fetchOrderProducts());
   }, [dispatch, productId]);
-//end malcolm added fetch all orders for add to cart
+  //end malcolm added fetch all orders for add to cart
 
   const handleSubmit = evt => {
     evt.preventDefault();
     const idd = product.products.id;
-    dispatch(updateSingleProductAsync({  name, price, quantity, idd }));
+    dispatch(updateSingleProductAsync({ name, price, quantity, idd }));
     setName('');
     setPrice('');
     setQuantity('');
   };
 
-  console.log("productid", typeof(parseInt(productId)))
+  console.log('productid', typeof parseInt(productId));
   //MALCOLM AM EDITS CART
-  const userId = useSelector((state) => state.auth.me.id);
-  console.log("userID:", userId)
+  const userId = useSelector(state => state.auth.me.id);
+  console.log('userID:', userId);
   const handleCart = evt => {
     // evt.preventDefault();
-    const fulfilled = false
-    dispatch(addOrderAsync({ userId, fulfilled}));
-    dispatch(addOrderProductAsync({orderId:orderlength,productId}));
-    dispatch(fetchAllOrders())
-    console.log("***********",orderlength)
-    dispatch(fetchOrderProducts())
-    
+    const fulfilled = false;
+    dispatch(addOrderAsync({ userId, fulfilled }));
+    dispatch(addOrderProductAsync({ orderId: orderlength, productId }));
+    dispatch(fetchAllOrders());
+    console.log('***********', orderlength);
+    dispatch(fetchOrderProducts());
+
     // const orderlength = useSelector((state) => state.singleProduct.orders.length)
     // dispatch(addOrderProducts({ UserId, ProductId, fulfilled}));
   };
@@ -64,44 +64,56 @@ console.log("startlength", orderlength)
 
   return (
     <div>
-      {/* <img src={campus.imageUrl} className='campus-Images' /> */}
-      <h1> {product.products.name} </h1>
-      <h1> {product.products.price} </h1>
-      <p>{product.products.quantity}</p>
-      <button type='button' onClick={() => dispatch(handleCart)}>
-        New add to cart{' '}
-      </button>
-      <button type='button' onClick={() => dispatch(addtoCart(cart))}>
-        old add to cart{' '}
-      </button>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            type='text'
-            value={name}
-            onChange={e => setName(e.target.value)}
-            name='product-name'
-            placeholder='Enter Product Name'
-          />
-          <br />
-          <input
-            type='text'
-            value={price}
-            name='product-price'
-            onChange={e => setPrice(e.target.value)}
-            placeholder='Enter Product Price'
-          />
-          <br />
-          <input
-            type='text'
-            value={quantity}
-            name='product-quantity'
-            onChange={e => setQuantity(e.target.value)}
-            placeholder='Enter Product Quantity'
-          />
-          <br />
-          <button type='submit'>Submit</button>
-        </form>
+      <div className='product-single-element'>
+        <div>
+          {/* <img src={campus.imageUrl} className='campus-Images' /> */}
+          <h1> {product.products.name} </h1>
+          <h1> {product.products.price} </h1>
+          <p>{product.products.quantity}</p>
+        </div>
+      </div>
+      <div className='form-div'>
+        <button
+          type='button'
+          className='addToCartBttn'
+          onClick={() => dispatch(handleCart)}
+        >
+          New add to cart{' '}
+        </button>
+        <button type='button' onClick={() => dispatch(addtoCart(cart))}>
+          old add to cart{' '}
+        </button>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              value={name}
+              onChange={e => setName(e.target.value)}
+              name='product-name'
+              placeholder='Enter Product Name'
+            />
+            <br />
+            <input
+              type='text'
+              value={price}
+              name='product-price'
+              onChange={e => setPrice(e.target.value)}
+              placeholder='Enter Product Price'
+            />
+            <br />
+            <input
+              type='text'
+              value={quantity}
+              name='product-quantity'
+              onChange={e => setQuantity(e.target.value)}
+              placeholder='Enter Product Quantity'
+            />
+            <br />
+            <button type='submit' className='addToCartBttn'>
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
