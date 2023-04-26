fadfda6fd9171fc2c8c3a0d69abf7ed506dd9170
Joseph-WhiteSWE <white.joseph.jr@gmail.com>
2023-04-26 14:10:55 -0500

    Fix cart bug Changes to be committed: modified:   client/features/auth/cart.js

--- client/features/auth/cart.js
+++ client/features/auth/cart.js
@@ -1,24 +1,20 @@
-
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 import {
   fetchAllOrders,
   fetchOrderProducts,
   deleteOrderAsync,
   deleteOrderProductAsync,
+} from '../../features/singleProductSlice';
 
-} from "../../features/singleProductSlice";
-
-import { fetchAllProducts } from "../../features/allProductsSlice";
-
+import { fetchAllProducts } from '../../features/allProductsSlice';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { removeFromCart, addToCart } from '../singleProductSlice';
 
 function Cart() {
-
   const handleCheckout = () => {
-    window.location.href = "/payment";
+    window.location.href = '/payment';
   };
 
   //Malcolm overnight cart edits//
@@ -28,20 +24,17 @@ function Cart() {
     dispatch(fetchAllProducts());
   }, [dispatch]);
 
-  const orderproducts = useSelector(
-    (state) => state.singleProduct.orderproducts
-  );
-  const orders = useSelector((state) => state.singleProduct.orders);
-  const products = useSelector((state) => state.products);
+  const orderproducts = useSelector(state => state.singleProduct.orderproducts);
+  const orders = useSelector(state => state.singleProduct.orders);
+  const products = useSelector(state => state.products);
 
-  console.log("orders", orders);
-  console.log("orderproducts", orderproducts);
-  console.log("products", products);
+  console.log('orders', orders);
+  console.log('orderproducts', orderproducts);
+  console.log('products', products);
 
   const dispatch = useDispatch();
 
-  const handleDelete = (id) => {
-
+  const handleDelete = id => {
     dispatch(deleteOrderAsync(id));
     dispatch(deleteOrderAsync(id));
     dispatch(fetchAllOrders());
@@ -49,12 +42,12 @@ function Cart() {
     dispatch(fetchAllProducts());
   };
 
-
-  const productinjector = (tt) => {
-    console.log(tt);
-    console.log(products[tt - 1]["name"]);
-    return products[tt - 1]["name"];
-
+  const productinjector = tt => {
+    try {
+      // console.log(tt)
+      // console.log((products[tt-1]["name"]))
+      return products[tt - 1]['name'];
+    } catch {}
   };
 
   const totalCart = () => {
@@ -69,22 +62,18 @@ function Cart() {
     <div>
       <h2 className='cart'>Your Cart</h2>
 
-
-      {orderproducts.map((product) => {
+      {orderproducts.map(product => {
         return (
           <div key={product.id}>
-
             <h3>
               Order id:{product.productId}__productName__
               {productinjector(product.productId)}
             </h3>
             <button
-
-              type="button"
+              type='button'
               onClick={() => handleDelete(product.productId)}
             >
               Remove product
-
             </button>
           </div>
         );
